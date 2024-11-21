82e109ae7ef174ff6cc57fd9513a607062a1b849
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-21 23:12:13 +0700

    Assignment 2 Array 2.

--- surveyResultArray2.java
+++ surveyResultArray2.java
@@ -8,21 +8,20 @@ public class surveyResultArray2 {
         for (int i = 0; i < 10; i++) {
             System.out.println("\nResponden " + (i+1));
             for (int j = 0; j < 6; j++) {
-                System.out.print("Pertanyaan " + (j+1)+": ");
-                surveyData[i][j]=input.nextInt();
-                   
+                System.out.print("Pertanyaan " + (j+1)+"(1-5): ");
+                surveyData[i][j]=input.nextInt();      
             }
-             
-            
         }
         System.out.println();
-        System.out.println("Survey Result: ");
+        System.out.println("Average value per Responden: ");
             for (int j = 0; j < 10; j++) {
-                System.out.println("Responden " + (j+1));
+            System.out.println("Responden " + (j+1));
+            int sum = 0;
                 for (int j2 = 0; j2 < 6; j2++) {
-                    System.out.print( "\nPertanyaan " + (j2+1)+": " + surveyData[j] [j2]+ " ");
+                sum += surveyData[j][j2];
                 }
+                double averageValue = (double) sum / 6;
+                System.out.println("Average value " + averageValue);
             }
-        
     }
 }
