c1083920b601966391b598b1319d43485d2b6b3a
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-23 10:02:49 +0700

    Experiment 1 Question 12 Jobsheet 12.

--- Cinema17.java
+++ Cinema17.java
@@ -23,9 +23,8 @@ public class Cinema17 {
         System.out.printf("%s \t %s \n", audience[2][0], audience[2][1]);
         System.out.printf("%s \t %s \n", audience[3][0], audience[3][1]);
 
-        System.out.println("Audience in the row 3: ");
-        for (String i : audience[2]) {
-            System.out.println(i);
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Audience in the row: " + (i+1) + String.join( ", ", audience[i]));
         }
     }
 }
