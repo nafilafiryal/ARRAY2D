10e8ec0eae2b296f0f4baa80df8950aff5114067
nafilafiryal <nafilahfiryal05@gmail.com>
2024-11-23 21:25:27 +0700

    Assignment Jobsheet 12.

--- Numbers17.java
+++ Numbers17.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
+
 public class Numbers17 {
     public static void main(String[] args) {
-        int[][] myNumber = new int[3][];
-        myNumber[0]=new int[5];
-        myNumber[1]=new int[3];
-        myNumber[2]=new int[1];
-        
+        int[][] myNumber = new int[3][5];
+        for (int i = 0; i < myNumber.length; i++) {
+            for (int j = 0; j < myNumber.length; j++) {
+                myNumber[i][j]=(i+1)*(j+1);
+
+            }
+            for (int j = 0; j < myNumber.length; j++) {
+                System.out.println(Arrays.toString(myNumber[i]));
+            }
+            for (int j = 0; j < myNumber.length; j++) {
+                System.out.println("Length of row " + (i+1) + ": " + myNumber[i].length);
+            }
+        }
     }
 }
--- surveyResultArray2.java
+++ surveyResultArray2.java
@@ -13,14 +13,46 @@ public class surveyResultArray2 {
             }
         }
         System.out.println();
-        System.out.println("Overall Average value: ");
+        System.out.println("Survey Result: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Responden " + (i+1));
+            for (int j = 0; j < 6; j++) {
+                System.out.println("Question " + (j+1) + ": " + surveyData[i][j]);
+            }
+        }
+        System.out.println("\nAverage value per Respondent");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Responden " + (i+1));
+            int sum = 0;
+            for (int j = 0; j < 6; j++) {
+                sum+=surveyData[i][j];
+
+            }
+            double averageValue = (double) sum /6;
+            System.out.println("Average value respondent " + (i+1) + averageValue);
+        }
+        
+
+        System.out.println("\nAverage value per Question ");
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Question " + (i+1));
+            int sum =0;
+            for (int j = 0; j < 10; j++) {
+                sum += surveyData[j][i];
+            }
+            double averageValue = (double) sum / 10;
+            System.out.println("Average value question "+ (i+1) + averageValue);
+        }
+        
+
+        System.out.println("\nOverall Average value: ");
             for (int j = 0; j < 10; j++) {
             int totalSum = 0;
                 for (int j2 = 0; j2 < 6; j2++) {
                 totalSum += surveyData[j][j2];
                 }
                 double overallAverage = (double) totalSum / (10*6);
-                System.out.print("\n" + overallAverage + " ");
+                System.out.print("\nOverall Average value respondent" + (j+1) + overallAverage + " ");
             }
     }
 }
